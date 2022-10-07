b7cab489911a34f551b702bf5d4590bdc13cd81b
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-10-06 19:33:54 -0700

    update feature

--- errors.c
+++ errors.c
@@ -1,97 +1,67 @@
-#include monty.h
+#include "monty.h"
+
+int usage_error(void);
+int malloc_error(void);
+int f_open_error(char *filename);
+int unknown_op_error(char *opcode, unsigned int line_number);
+int no_int_error(unsigned int line_number);
+
 /**
- * push_error - pushs errors
- * @err_code: error code
+ * usage_error - Prints usage error messages.
  *
- * Return: void
+ * Return: (EXIT_FAILURE) always.
  */
-void push_error(short int err_code)
+int usage_error(void)
 {
-	switch (err_code)
-	{
-		case 11:
-			fprinf(stderr, "Error: malloc failed\n");
-			free_data();
-			break;
-		case 12:
-			fprinf(stderr, "USAGE: monty file\n");
-			break;
-		case 14:
-			fprintf(stderr, "Error: can't open file %s\n", data.filename);
-			break;
-		case 15:
-			fprintf(stderr, "L%d: unknown instruction%s\n",
-					data.line_number, data.args[0]);
-			free_data();
-			break;
-		case 16:
-			fprintf(stderr, "L%d: usage: push integer\n", data.line_number);
-			free_data();
-			break;
-		case 17:
-			fprintf(stderr, "L%d: can't pint, stack empty\n", data.line_number)
-			free_data();
-			break;
-		case 18:
-			fprintf(stderr, "L%d: can't pop an empty stack\n", data.line_number);
-			free_data();
-			break;
-		case 19:
-			fprintf(stderr, "L%d: can't swap, stack too short\n", data.line_number);
-			free_data();
-			break;
-		default:
-			break;
-	}
+	fprintf(stderr, "USAGE: monty file\n");
+	return (EXIT_FAILURE);
 }
+
 /**
- * get_error_2 - gets error
- * @err_code: error code
+ * malloc_error - Prints malloc error messages.
  *
- * Return: void
+ * Return: (EXIT_FAILURE) always.
  */
-void get_error_2(short int err_code)
+int malloc_error(void)
 {
-	switch (err_code)
-	{
-		case 20:
-			fprintf(stderr, "L%d: can't add, stack too short\n", data.line_number);
-			free_data();
-			break;
-		case 21:
-			fprintf(stderr, "L%d: can't sub, stack too short\n", data.line_number);
-			free_data();
-			break;
-		case 22:
-			fprintf(stderr, "L%d: can't div, stack too short\n", data.line_number);
-			free_data();
-			break;
-		case 23:
-			fprintf(stderr, "L%d: division by zero\n", data.line_number);
-			free_data();
-			break;
-		case 24:
-			fprintf(stderr, "L%d: can't mul, stack too short\n", data.line_number);
-			free_data();
-			break;
-		case 25:
-			fprintf(stderr, "L%d: can't mod, stack too short\n", data.line_number);
-			free_data();
-			break;
-		default:
-			break;
-	}
+	fprintf(stderr, "Error: malloc failed\n");
+	return (EXIT_FAILURE);
 }
+
 /**
- * push_error - pushes error
- * @err_code: error code
- * Return: void
+ * f_open_error - Prints file opening error messages w/ file name.
+ * @filename: Name of file failed to open
+ *
+ * Return: (EXIT_FAILURE) always.
  */
-void push_error(short int err_code)
+int f_open_error(char *filename)
 {
-	if (err_code >= 10 && err_code < 20)
-		get_error_1(err_code);
-	else if (err_code >= 20 && err_code < 30)
-		get_error_2(err_code);
-	exit(EXIT_FAILURE);
+	fprintf(stderr, "Error: Can't open file %s\n", filename);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * unknown_op_error - Prints unknown instruction error messages.
+ * @opcode: Opcode where error occurred.
+ * @line_number: Line number in Monty bytecodes file where error occured.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int unknown_op_error(char *opcode, unsigned int line_number)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n",
+		line_number, opcode);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * no_int_error - Prints invalid monty_push argument error messages.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int no_int_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	return (EXIT_FAILURE);
 }
--- main.c
+++ main.c
@@ -1,40 +1,29 @@
 #include "monty.h"
-/**
- * Globale data - collection of data instance
- */
-data_t data;
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char **op_toks = NULL;
 
 /**
- * main - main function
- * @ac: arg count
- * @av: args
- * Return: (Success) EXIT_SUCCESS
- * --------(Fail) EXIT_FAILURE
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	stack_t *stack = NULL;
-	ssize_t n_read = 1;
-	size_t length = 0;
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
-	memset((void *) &data, 0, sizeof(data));
-	if (ac != 2)
-		push_error(12)
-	data.filename = av[1];
-	data.fp = fopen(data.filename, "r");
-	if (data.fp == NULL)
-		push_error(14);
-	while ((n_read = getline(&data.line, &length, data.fp)) > 0)
-	{
-		if (*data.line == '\n')
-			continue;
-		data.line_number++;
-		free(data.args);
-		if (split_line() < 0)
-			continue;
-		process_line(&stack);
-	}
-	free_data();
-	free_dlistint(stack);
-	return (EXIT_SUCCESS);
+	if (argc != 2)
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
 }
