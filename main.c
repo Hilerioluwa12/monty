d68d99ed968ba1f3a23b58bb4b8b71d21a100f3e
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-10-06 19:53:04 -0700

    update features

--- main.c
+++ main.c
@@ -1,19 +1,5 @@
 #include "monty.h"
-<<<<<<< HEAD
 
-/**
- * Globale data - collection of data instance
- */
-data_t data;
-
-/**
- * main - main function
- * @ac: arguments count
- * @av: arguments vector
- *
- * Return: (Success) EXIT_SUCCESS
- * ------- (Fail) EXIT_FAILURE
-=======
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -27,38 +13,9 @@ char **op_toks = NULL;
  * @argv: pointer to an array of char pointers to arguments
  *
  * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
->>>>>>> Ileri12
  */
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
-	ssize_t n_read = 1;
-	size_t length = 0;
-	stack_t *stack = NULL;
-
-	memset((void *) &data, 0, sizeof(data));
-	if (ac != 2)
-		push_error(12);
-	data.filename = av[1];
-	data.fp = fopen(data.filename, "r");
-	if (data.fp == NULL)
-		push_error(14);
-	while ((n_read = getline(&data.line, &length, data.fp)) > 0)
-	{
-		if (*data.line == '\n')
-			continue;
-		data.line_number++;
-		free(data.args);
-		if (split_line() < 0)
-			continue;
-		if (*data.args == NULL)
-			continue;
-		process_line(&stack);
-	}
-	free_data();
-	free_dlistint(stack);
-	return (EXIT_SUCCESS);
-=======
 	FILE *script_fd = NULL;
 	int exit_code = EXIT_SUCCESS;
 
@@ -70,5 +27,4 @@ int main(int argc, char **argv)
 	exit_code = run_monty(script_fd);
 	fclose(script_fd);
 	return (exit_code);
->>>>>>> Ileri12
 }
